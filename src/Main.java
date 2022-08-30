e0f554cc27d73eef52dd9d35b78a132589650ef6
millerladino <millerladino@gmail.com>
2022-08-30 18:02:13 -0500

    subida del ejecicio de ciclos y condicionales

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,59 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args){
-        suma(5, 3 ,12);
-        Coche miCoche = new Coche();
-        miCoche.AddPuerta();
-        System.out.println("mi Coche ahora tiene " +miCoche.puertas +" puerta");
-    }
-    public static void suma(int a, int b, int c){
-        int resultado;
-        resultado = a + b + c;
-        System.out.println(resultado);
-    }
-}
+        int numero = -214;
+        int number = 1;
+        int number2 = 2;
+        String estacion = "PAZ";
 
-class Coche{
-    public int puertas = 0;
+        System.out.println("-----------------------ejercicio 1 IF-----------------------");
 
-    public void AddPuerta(){
-        this.puertas++;
+        if(numero >= 1){
+            System.out.println("el numero ingresado es positivo, el numero es: " + numero);
+        }else if(numero == 0){
+            System.out.println("El numero ingresado es Cero, el numero es: " + numero);
+        }else{
+            System.out.println("El numero ingresado es negativo, el numero es:" + numero);
+        }
+        System.out.println("-----------------------ejercicio 2 WHILE-----------------------");
+
+        while(number < 3){
+            System.out.println(number);
+            number++;
+        }
+        System.out.println("-----------------------ejercicio 3 DO-WHILE-----------------------");
+
+        do{
+            System.out.println(number2);
+            number2++;
+        }while(number2 < 3);
+
+        System.out.println("-----------------------ejercicio 4 FOR------------------------");
+
+        for(int number3=0; number3 < 3; number3++){
+            System.out.println(number3);
+        }
+
+        System.out.println("-----------------------ejercicio 4 SWITCH------------------------");
+
+        switch(estacion){
+            case "PRIMAVERA":
+                System.out.println("Es PRIMAVERA");
+                break;
+            case "VERANO":
+                System.out.println("Es VERANO");
+                break;
+            case "OTONO":
+                System.out.println("Es OTONO");
+                break;
+            case "INVIERNO":
+                System.out.println("Es INVIERNO");
+                break;
+            default:
+                System.out.println("Esta no es una estacion del anio");
+
+        }
+
     }
-}
+
+    }
